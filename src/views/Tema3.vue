2be64b3aef1977b4168b57f860f85acff21c9733
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2021-10-12 22:20:46 -0500

    Ajustes

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,6 +6,261 @@
       .titulo-principal__numero
         span 3
       h1 Modelos de producción
+
+    p.mb-5 Antes de empezar a hablar sobre producción es muy importante definirla como el estudio de las técnicas de gestión empleadas para conseguir la mayor diferencia entre el valor agregado y el costo incorporado consecuencia de la transformación de recursos en productos finales.
+    .row.justify-content-center.mb-5
+      .col-lg-4 
+        h4.mb-0 Existen diferentes modelos de producción según los sectores económicos, primario, secundario y terciario. 
+      .col-lg-8 
+        p.mb-0 Se podría decir que la industria del diseño y la moda se encuentra en estos últimos dos, no solo porque transforma la materia prima, sino que también se le presta un servicio intangible por medio de las redes comerciales de productos en el mundo al consumidor final. 
+    figure.mb-5
+      img(src='@/assets/curso/tema3/img01.png')      
+
+    p.mb-5 Los modelos de producción en la industria de la moda integran principios y fundamentos conceptuales que deben ser apropiados por el personal que trabaja en el sector, a continuación, se hace referencia de ciertos términos que son importante conocer. 
+
+    h3 Modelos de producción
+    p.mb-5 Los sistemas de producción se dividen en producción continua y en sistemas de producción intermitente, ambos a su vez se dividen en otros subsistemas; la producción continua se divide en producción en masa y producción por procesos, la producción intermitente se divide en sistemas de producción modular, por lotes y por proyectos.
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        figure
+          img(src='@/assets/curso/tema3/img02.svg')   
+
+    h3 Fundamentos conceptuales para procesos de producción   
+    p.mb-5 Los modelos de producción en la industria de la moda integran principios y fundamentos conceptuales que se manejan deben ser apropiados por el personal que trabaja en el sector, a continuación, se hace referencia de ciertos términos que son importante conocer
+    
+    div.mb-5
+      p EL componente
+
+    h3 La planificación de la producción
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        LineaTiempoD.color-primario
+          div(numero="1" titulo="Planificación de la producción")   
+            .row.justify-content-center
+              .col-lg-5
+                figure
+                  img(src='@/assets/curso/tema3/img03.png')   
+              .col-lg-7
+                p.mb-4 Está determinada por la unidad a vender en un periodo de tiempo determinado, con base en esta información se analiza la capacidad instalada de la planta, en cuanto a maquinaria, capital humano, tiempos de producción, suministro de materiales, tiempo de las operaciones, distribución de los módulos, según  complejidad de las prendas a fabricar. 
+                p.mb-0 La organización de las plantas de producción está determinada por el flujo del proceso productivo, la flexibilidad en la ubicación de los puestos de trabajo, el flujo continuo de las operaciones, la comodidad de los trabajadores, la iluminación y aireación en beneficio del trabajador, las distancias en cada una de las operaciones, y la funcionalidad de manera efectiva.
+          div(numero="2" titulo="Organización por módulos (Celdas) de producción en confecciones")           
+            p.mb-4 La manufactura modular o celda de producción es la forma de organización más utilizada en plantas de producción en confecciones, es un concepto desarrollado por Toyota en los años 40's, su principio fundamental es el justo a tiempo, la reducción de inventarios, flexibilidad en los cambios de referencia, reducción de los costos y ciclos de producción a partir de la optimización de los tiempos  y la mejora de los indicadores de productividad. 
+            .row.justify-content-center            
+              .col-lg-5
+                figure
+                  img(src='@/assets/curso/tema3/img04.png')   
+              .col-lg-7
+                p.mb-4 El módulo o celda de producción se organiza de manera lineal de acuerdo con el orden operacional definido para el producto(vestuario), en un puesto de trabajo se realiza una o dos operaciones según el tipo de maquinaria, tiempo de producción y habilidad del operario.  Se tienen mesas auxiliares para descargar la producción terminada que será entregada al operario siguiente. 
+                p.mb-0 Este sistema de producción se caracteriza por lotes de productos de una misma serie o referencia, por lo general son pequeños en el número de unidades, debido a las tendencias de la moda según la temporada. Su forma de organización es en pequeños módulos de producción conformado entre 3 y 6 operarios, cada uno encargado de una o varias operaciones.
+          div(numero="3" titulo="El flujo de producción")
+            .row.justify-content-center
+              .col-lg-5
+                figure
+                  img(src='@/assets/curso/tema3/img05.png')   
+              .col-lg-7
+                p.mb-4 El flujo de producción en los sistemas modulares es en forma de U de acuerdo con el orden operacional definido en la ficha técnica del producto(vestuario), En ocasiones quien rota es el operario según su habilidad y conocimiento en la operación y manejo de la maquinaria. 
+                p.mb-0 Para su implementación se deben tener en cuenta aspectos como cohesión del grupo, definición de normas y roles, determinar los objetivos de producción para el módulo, distribución y asignación de tarea y cargas de trabajo, compresión de la interrelación de sus operaciones y operarios. <b>La meta de uno es la meta de todos.</b>                            
+        
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        div.t3-div.py-5
+          .row.justify-content-start.mb-2
+            .col-lg-1
+            .col-lg-1.p-0
+              figure
+                img(src='@/assets/curso/tema3/img06.svg')  
+            .col-lg-5.p-0
+              div.BorTit2.m-0.pb-0.mb-0
+                h2.m-0 La ficha
+              div.BorTit3.m-0.pt-0.mt-0
+                h1.m-0 técnica
+          .row.justify-content-center.mb-5
+            .col-lg-5
+              figure
+                img(src='@/assets/curso/tema3/img07.png')  
+            .col-lg-5
+              p.mb-0 La ficha técnica es un documento cuya función principal es informar de una manera estandarizada y sencilla las características técnicas de su producto, describiendo las operaciones, tiempos, maquinaria, las imágenes del producto, las lupas, las observaciones de diseño o calidad   y normas técnicas requerida para la elaboración de este.   Esta información tiene utilidad a nivel comercial y productivo. La ficha técnica cuenta con el orden específico en que se va realizando la prenda: diseño, patronaje, trazo, corte, tiqueteado, confección, y terminación. 
+          .row.justify-content-center.mb-5
+            .col-lg-10
+              figure
+                .video
+                  iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)      
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        div.t3-div.py-5
+          .row.justify-content-start.mb-2
+            .col-lg-1
+            .col-lg-1.p-0
+              figure
+                img(src='@/assets/curso/tema3/img08.svg')  
+            .col-lg-5.p-0
+              div.BorTit2.m-0.pb-0.mb-0
+                h2.m-0 La orden de 
+              div.BorTit3.m-0.pt-0.mt-0
+                h1.m-0 producción
+          .row.justify-content-center.mb-5
+            .col-lg-5
+              figure
+                img(src='@/assets/curso/tema3/img09.png')  
+            .col-lg-5
+              p.mb-0 La orden de producción es un documento que establece los tiempos en días, describe el total de unidades a elaborar por referencia, las condiciones de entrega, los tiempos programados por lotes según la cantidad de piezas que conforman cada prenda. Este documento permite el control individualizado que se lleva a cada pedido o trabajo que se está elaborado.
+          
+    h3 Tipos de maquinaria para el proceso productivo en la industria de la confección
+    .row.justify-content-center.mb-5
+      .col-10
+        .cajon.color-primario.py-4.px-5
+          .row.justify-content-center
+            .col-2
+              figure
+                img(src='@/assets/curso/tema3/img10.svg')              
+            .col-10.d-flex.align-items-center
+              p.mb-0 La identificación de los tipos de maquinaria del proceso productivo es primordial para la calidad en la costura de las prendas, cada máquina está dotada de aditamentos especializados según el tipo de tejido, prenda y tipo de operación. 
+    p.mb-5 La maquinaria utilizada en el proceso productivo de confección tiene varias características, entre ellas está el tipo de tecnología que va desde primera generación (máquinas mecánicas), segunda (maquina eléctricas), tercera (electrónicas)  y cuarta generación (autómatas), cada máquina ofrece  ventajas para el confeccionista, razón por la cual  una persona que quiera trabajar en el mundo de la moda debe adquirir este conocimiento, porque le permitirá ser más creativo y realista en el desarrollo de sus prendas. Las máquinas más utilizadas en los diferentes procesos de confección son: maquina plana, maquina plana dos agujas, maquina fileteadora, maquina recubridora y maquina presilladora.
+
+    .row.justify-content-center.mb-5
+      .col-7
+        figure
+          img(src='@/assets/curso/tema3/img11.png')   
+
+    h3 Métodos y tiempos    
+    p.mb-4 En la industria de la confección hablar de rapidez en los procesos permite pensar siempre que hay una forma correcta para hacer las cosas y a su vez eficiente, es aquí donde los conceptos de métodos y tiempos toman poder.
+    p.mb-5 En cualquier actividad que se realice y más aún en el desarrollo de colecciones, se necesita utilizar métodos adecuados para las diferentes operaciones y permitir ejecutarlas con mayor precisión, esta habilidad se desarrolla a través del conocimiento que se adquiere por el trabajo repetitivo; el saber estandarizar un proceso o una prenda de vestir permite identificar fechas de entrega y poder adquirir compromisos con los clientes internos y externos de la compañía.          
+
+    .row.justify-content-center.mb-5
+      .col-10
+        div.t3-div2.py-4 
+          .row.justify-content-center
+            .col-lg-2
+              figure
+                img(src='@/assets/curso/tema3/img12.svg')   
+            .col-lg-8
+              h4 Los métodos
+              p.mb-0 Los métodos son un conjunto de técnicas para realizar una actividad en un proceso productivo, determinando de forma clara, cuál es la mejor manera de realiza un trabajo, tienen como finalidad mejorar la productividad en una empresa.  Los métodos de trabajo siempre tendrán influencia en los tiempos de operación de una prenda, por lo tanto, para mejorar los tiempos se debe mejorar los métodos de trabajo.
+
+    .row.justify-content-center.mb-5
+      .col-10
+        div.t3-div2.py-4 
+          .row.justify-content-center
+            .col-lg-2
+              figure
+                img(src='@/assets/curso/tema3/img13.svg')   
+            .col-lg-8
+              h4 Los tiempos
+              p.mb-0 Son los minutos requeridos para fabricar un producto o prestar un servicio. En la industria de la confección estos dos elementos son fundamentales, porque los costos en los procesos de producción en confecciones además de los materiales están determinados por los tiempos de producción.
+
+    .row.justify-content-center.mb-5
+      .col-10
+        div.t3-div2.py-4 
+          .row.justify-content-center
+            .col-lg-2
+              figure
+                img(src='@/assets/curso/tema3/img14.svg')   
+            .col-lg-8
+              h4 Tiempo estándar (SAM)
+              p.mb-0 Los tiempos SAM o tiempo estándar, es el tiempo que se demora la fabricación de una prenda una vez realizado un análisis de tiempos. En la industria de la confección el mercado ya tiene identificado los tiempos en los cuales se debe fabricar una prenda. Este conocimiento es fundamental para un diseñador porque a mayor complejidad en el diseño,  se requiere más  tiempo de fabricación  y por ende mayor  costo de la prenda. 
+
+    .row.justify-content-center.mb-5
+      .col-6
+        figure
+          img(src='@/assets/curso/tema3/img16.svg') 
+
+    p.mb-5 A continuación, conoceremos algunos conceptos importantes para entender a mayor profundidad un estudio de tiempos. 
+    h3 Tiempos en los procesos de producción
+    p.mb-5 Los tiempos son calculados de acuerdo con las operaciones y es el área de ingeniería la encargada de realizar los estudios de métodos y tiempos para balancear una planta de producción.
+
+    TabsA.color-acento-contenido.mb-5      
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Medición del trabajo")
+        h4 Medición del trabajo
+        .row
+          .col-lg-2
+            figure
+              img(src='@/assets/curso/tema3/img17.svg') 
+          .col-lg-10.d-flex.align-items-center
+            p.mb-0 Es la aplicación de técnicas para determinar el tiempo que invierte un trabajador en llevar a cabo una tarea definida efectuándose según una norma de ejecución preestablecida.
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Estudio de tiempos")
+        h4 Estudio de tiempos
+        p.mb-5 Es una técnica empleada para registrar los tiempos y ritmos de trabajo correspondientes a los elementos de una tarea definida, efectuada en condiciones determinadas y para analizar los datos a fin de averiguar el tiempo requerido para efectuar la tarea según una norma de ejecución establecida.
+        h4 Etapas del estudio de tiempos:
+        figure
+          img(src='@/assets/curso/tema3/img18.svg') 
+
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Tiempo real")
+        h4 Tiempo real
+        .row
+          .col-lg-4
+            div.BorTit
+              p.mb-0 Total minutos máquina y día
+            figure
+              img(src='@/assets/curso/tema3/img19.svg') 
+          .col-lg-8.d-flex.align-items-center
+            p.mb-0 Se define como el tiempo medio del elemento empleado realmente por el operario durante un estudio de tiempos.
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Tiempo suplementario")
+        h4 Tiempo suplementario
+        .row
+          .col-lg-2           
+            figure
+              img(src='@/assets/curso/tema3/img20.svg') 
+          .col-lg-10.d-flex.align-items-center
+            p.mb-0 Es el tiempo que se le concede al trabajador con el objeto de compensar la fatiga, las necesidades fisiológicas y los imprevistos, que se presentan en el desarrollo de sus tareas. 
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Tiempos estándar")
+        h4 Tiempos estándar
+        p.mb-5 Es el tiempo que se determina necesario para que un trabajador calificado, trabajando a un ritmo normal, con fatigas y demoras normales, realice una cantidad definida de trabajo con una calidad especificada, siguiendo los métodos establecidos.
+        .row.justify-content-start
+          .col-10
+            .tabla-a.color-acento-contenido
+              table            
+                thead
+                  tr
+                    th Elemento
+                    th Ttc (Tiempo Total Concedido)                    
+                tbody.text-center(style="background-color:white")
+                  tr
+                    td A
+                    td 1.113
+                  tr
+                    td B
+                    td 2.106
+                  tr
+                    td C
+                    td 1.590
+                  tr
+                    td D
+                    td 3.520
+                  tr
+                    td E
+                    td 1.008
+                  tr
+                    td F
+                    td 1.464
+                  tr
+                    td Tiempo Estándar (Σ(Ttc))
+                    td 10.345                  
+
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Trabajador calificado")
+        h4 Estudios de tiempo con cronómetro
+        .row                     
+          .col-lg-12.d-flex.align-items-center
+            p.mb-0 Es aquel que tiene experiencia, los conocimientos y otras cualidades necesarias para efectuar el trabajo en curso según normas satisfactorias de seguridad, cantidad y calidad. 
+        
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Estudio de tiempos con cronómetro")
+        h4 Estudios de tiempo con cronómetro
+        .row
+          .col-lg-2           
+            figure
+              img(src='@/assets/curso/tema3/img21.svg') 
+          .col-lg-10.d-flex.align-items-center
+            p.mb-0 Este método para establecer estándares de tiempo se basa en los resultados de la observación directa del tiempo de una tarea tal como se efectúa. Este tiempo se ajusta para reflejar el ritmo y la habilidad de un trabajador promedio, al cual llamamos tiempo normal. Finalmente, el tiempo normal se modifica para reflejar cualquier tiempo adicional requerido (tolerancias) para obtener así el tiempo estándar final. 
+
+
+    .row.justify-content-center
+      .col-7
+        a.anexo.mb-4.mb-lg-0(href="https://en.wikipedia.org/wiki/Main_Page" target="_blank")
+          .anexo__icono
+            img(src="@/assets/template/icono-link.svg")
+          .anexo__texto
+            p.mb-0 Work Study on Textile Industry. Para Ampliar la información de cómo se analiza en una empresa de confecciones lo invitamos a ver "Estudio de tiempos en industria textil confección".
 </template>
 
 <script>
